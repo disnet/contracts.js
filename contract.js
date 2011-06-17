9f57c6378a30a150041d2c78ee4ec286b7090c70
Tim Disney <tim.disney@gmail.com>
2011-06-17 15:12:35 -0700

    basic object contracts

--- contract.js
+++ contract.js
@@ -29,6 +29,59 @@ var C = (function() {
         };
       };
     },
+    object: function(oc) {
+      return function(pos, neg) {
+        return function(obj) {
+          return Proxy.create({
+            getOwnPropertyDescriptor: function(name) {
+              var desc = Object.getOwnPropertyDescriptor(obj, name);
+              if (desc !== undefined) { desc.configurable = true; }
+              return desc;
+            },
+            getPropertyDescriptor: function(name) {
+              var desc = Object.getPropertyDescriptor(obj, name); 
+              if (desc !== undefined) { desc.configurable = true; }
+              return desc;
+            },
+            getOwnPropertyNames: function() {
+              return Object.getOwnPropertyNames(obj);
+            },
+            getPropertyNames: function() {
+              return Object.getPropertyNames(obj);               
+            },
+            defineProperty: function(name, desc) {
+              Object.defineProperty(obj, name, desc);
+            },
+            delete: function(name) { return delete obj[name]; },   
+            fix: function() {
+              if (Object.isFrozen(obj)) {
+                return Object.getOwnPropertyNames(obj).map(function(name) {
+                  return Object.getOwnPropertyDescriptor(obj, name);
+                });
+              }
+              return undefined;
+            },
+            has: function(name) { return name in obj; },
+            hasOwn: function(name) { return Object.prototype.hasOwnProperty.call(obj, name); },
+            get: function(receiver, name) {
+              // interesting stuff here
+              return oc[name](pos, neg)(obj[name]);
+            },
+            set: function(receiver, name, val) {
+              // interesting stuff here
+              obj[name] = oc[name](pos, neg)(val);
+              return true;
+            }, 
+            enumerate: function() {
+              var result = [];
+              for (name in obj) { result.push(name); };
+              return result;
+            },
+            keys: function() { return Object.keys(obj) }
+          });
+        };
+      };
+    },
     any: function(pos, neg) {
       return function(val) {
         return val;
@@ -82,14 +135,21 @@ var K = (function() {
 
 
 var M = (function () {
-  // wrapping the math library in contracts
   function badAbs(x) {
     return x;
   }
   function id(x) { return x; }
+
+  var o = {
+    id: id
+  };
+
   return {
     id: C.guard(C.fun(C.any, C.any), id, "server", "client"),
     idNone: C.guard(C.fun(C.none, C.none), id, "server", "client"),
+    idObj: C.guard(C.object({
+      id: C.fun(K.Number, K.Number)
+    }), o, "server", "client"),
     abs: C.guard(C.fun(K.Number, C.and(K.Number, K.Pos)), Math.abs, "server", "client"),
     badAbs: C.guard(C.fun(K.Number, C.and(K.Number, K.Pos)), badAbs, "server", "client") 
   }
@@ -110,7 +170,9 @@ var M = (function () {
     {f: M.idNone, a:[3], b: true},
     {f: M.abs, a: [4], b: false},
     {f: M.badAbs, a: [-4], b: true},
-    {f: M.abs, a: ["hi"], b: true}
+    {f: M.abs, a: ["hi"], b: true},
+    {f: M.idObj["id"], a: [3], b: false},
+    {f: M.idObj["id"], a: ["hi"], b: true}
   ];
   for(i = 0; i < tests.length; i++) {
     var test = tests[i];
