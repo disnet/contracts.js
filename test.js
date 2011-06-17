e1f2668ac7c03e200e48587d6ed820c1d9271ded
Tim Disney <tim.disney@gmail.com>
2011-06-17 15:12:22 -0700

    we'll come back to this

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
-var T = (function () {
+// var T = (function () {
 
-  function id(x) {
-    return x;
-  }
+//   function id(x) {
+//     return x;
+//   }
 
-  function badId(x) {
-    return x+1;
-  }
+//   function badId(x) {
+//     return x+1;
+//   }
 
-  return {
-    id: C.wrap(id),
-    badId: C.wrap(badId)
-  }
+//   return {
+//     id: C.wrap(id),
+//     badId: C.wrap(badId)
+//   }
 
-})();
-
-
-function assert(b) {
-  if (!b) {
-    throw {
-      name: "AssertError",
-      message: "Failed assert"
-    }
-  }
-}
+// })();
 
 
-(function test() {
-  assert(T.id(4) === 4);
-  assert(T.badId(4) === 4);
-  return "passed all tests";
-})();
+// function assert(b) {
+//   if (!b) {
+//     throw {
+//       name: "AssertError",
+//       message: "Failed assert"
+//     }
+//   }
+// }
+
+
+// (function test() {
+//   assert(T.id(4) === 4);
+//   assert(T.badId(4) === 4);
+//   return "passed all tests";
+// })();
