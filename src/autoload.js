92ec2acff6a247bbb5d172380286c5d99fd28820
Tim Disney <tim.disney@gmail.com>
2011-08-22 14:45:21 -0700

    adding separate file to load contracts info scope

---
+++ src/autoload.js
@@ -0,0 +1,10 @@
+// load all the contract identifiers into the global scope
+function load(obj) {
+  var name;
+  for(name in obj) {
+    if(obj.hasOwnProperty(name)) {
+      window[name] = obj[name];
+    }
+  }
+}
+load(Contracts.contracts);
