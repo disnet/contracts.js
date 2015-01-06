e9bfb420df2423e78da354e989fc4b701828d8fd
Leo Meyerovich <leo@graphistry.com>
2015-01-05 21:08:42 -0500

    Eliminate use of bind that was tripping up jshint

--- src/helper-macros.js
+++ src/helper-macros.js
@@ -1,12 +1,6 @@
 let if = macro {
     rule { $cond:expr then $then:expr else $else:expr } => {
-        (function() {
-            if ($cond) {
-                return $then
-            } else {
-                return $else
-            }
-        }.bind(this))()
+        (($cond) ? ($then) : ($else))
     }
     // fall through
     rule { $rest ...} => { if $rest ...}
