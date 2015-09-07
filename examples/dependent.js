4afe7b8802277f36fce040741ef941f0eb097fb6
Tim Disney <tim.disney@gmail.com>
2015-09-06 19:37:16 -0700

    makes sqrt example more right #27

--- examples/dependent.js
+++ examples/dependent.js
@@ -1,6 +1,7 @@
 import @ from "contracts.js"
 
-@ (x: Pos) -> res: Num | res <= x
+@ (x: Pos) -> res:Num | res > (Math.sqrt(x) - 0.1) &&
+                        res < (Math.sqrt(x) + 0.1)
 function bad_square_root(x) {
     // oops, square *root* not square!
     return x * x;
