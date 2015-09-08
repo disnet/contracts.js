3ad1335a8c92597f116d97a6da5b3359ce90fca0
Tim Disney <tim.disney@gmail.com>
2015-09-07 17:14:39 -0700

    remove use of math.sqrt in example #27

--- examples/dependent.js
+++ examples/dependent.js
@@ -1,7 +1,7 @@
 import @ from "contracts.js"
 
-@ (x: Pos) -> res:Num | res > (Math.sqrt(x) - 0.1) &&
-                        res < (Math.sqrt(x) + 0.1)
+@ (x: Pos) -> res:Num | x > (res * res - 0.1) &&
+                        x < (res * res + 0.1)
 function bad_square_root(x) {
     // oops, square *root* not square!
     return x * x;
